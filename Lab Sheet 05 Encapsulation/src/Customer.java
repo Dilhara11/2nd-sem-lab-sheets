6c7fd5f8cca2da6de10e37c2778253f9cd79e1a7
Achintha_Dilhara <achinthakaveen11@gmail.com>
2024-09-06 10:28:18 +0530

    encapsulation first question

--- Lab Sheet 05 Encapsulation/src/Customer.java
+++ Lab Sheet 05 Encapsulation/src/Customer.java
@@ -1,2 +1,44 @@
-package PACKAGE_NAME;public class Customer {
+public class Customer {
+    private int id;
+    private String name;
+    private char gender;
+
+    public Customer(int id, String name, char gender) {
+        this.id = id;
+        this.name = name;
+        this.gender = gender;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", gender=" + gender +
+                '}';
+    }
+
+
+}
+
+class TestCustomer{
+    public static void main(String[] args){
+        Customer customer = new Customer(123, "Achintha", 'M');
+        System.out.println(customer.getId());
+        System.out.println(customer.getName());
+        System.out.println(customer.getGender());
+        System.out.println(customer);
+    }
 }
